9e273d29a6455389c74c0f1c022bb2225bd3a985
combswo <combswo@miamioh.edu>
2024-08-24 15:45:00 -0400

    Initial push for hackathon code; connected to Heroku Postgres db

--- scripts/forms.py
+++ scripts/forms.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField
+from wtforms.validators import DataRequired, Length, Optional
 
 
 class LoginForm(Form):
-    username = StringField('Username:', validators=[validators.required(), validators.Length(min=1, max=30)])
-    password = StringField('Password:', validators=[validators.required(), validators.Length(min=1, max=30)])
-    email = StringField('Email:', validators=[validators.optional(), validators.Length(min=0, max=50)])
+    username = StringField('Username:', validators=[DataRequired(), Length(min=1, max=30)])
+    password = StringField('Password:', validators=[DataRequired(), Length(min=1, max=30)])
+    email = StringField('Email:', validators=[Optional(), Length(min=0, max=50)])
--- scripts/tabledef.py
+++ scripts/tabledef.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import sys
 import os
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-# Local
-SQLALCHEMY_DATABASE_URI = 'sqlite:///accounts.db'
+from sqlalchemy.orm import relationship
 
 # Heroku
-#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 Base = declarative_base()
 
@@ -35,5 +31,37 @@ class User(Base):
         return '<User %r>' % self.username
 
 
-engine = db_connect()  # Connect to database
-Base.metadata.create_all(engine)  # Create models
+class Search(Base):
+    __tablename__ = "search"
+
+    searchid = Column(Integer, primary_key=True)
+    longitude = Column(Float)
+    latitude = Column(Float)
+
+    # Relationship to Bycatch
+    bycatches = relationship("Bycatch", back_populates="search")
+
+    def __repr__(self):
+        return '<Search %r>' % self.searchid
+
+
+class Bycatch(Base):
+    __tablename__ = "bycatch"
+
+    id = Column(Integer, primary_key=True)
+    fish = Column(String(50))
+    index = Column(Integer)
+    searchid = Column(Integer, ForeignKey('search.searchid'))
+
+    # Relationship to Search
+    search = relationship("Search", back_populates="bycatches")
+
+    def __repr__(self):
+        return '<Bycatch %r>' % self.fish
+
+
+# Connect to the database
+engine = db_connect()
+
+# Create all tables in the database
+Base.metadata.create_all(engine)
